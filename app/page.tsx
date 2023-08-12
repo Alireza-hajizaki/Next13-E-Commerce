bfe77ff6404b15c1b840cf9a692d5d3e208eec48
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-08-12 22:36:26 +0330

    card products & rating & formatPrices completed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Container from "./components/Container";
 import HomeBanner from "./components/nav/HomeBanner";
 import ProductCard from "./components/products/ProductCard";
 import { products } from "./components/utils/products";
-import { truncateText } from "./components/utils/truncateText";
 
 export default function Home() {
   return (
@@ -14,7 +13,6 @@ export default function Home() {
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {products.map((product:any)=> {
           return <ProductCard data={product}/>
-          // return <div>{truncateText(product.name)}</div>
         })}
       </div>
     </Container>
