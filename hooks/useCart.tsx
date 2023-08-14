e159dfa3b8abe56e3b431980f3232fcd4603aa01
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-08-14 22:07:36 +0330

    clear all cart compeleted

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -9,6 +9,7 @@ type CartContextType = {
   handleRemoveProductFromCart : (product: CartProductType) => void
   handleCartQtyIncrease : (product: CartProductType) => void
   handleCartQtyDecrease : (product: CartProductType) => void
+  handleClearCart : () => void
 };
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -94,13 +95,20 @@ export const CartContextProvider = (props: Props) => {
     }
   } ,[cartProducts])
 
+  const handleClearCart = useCallback(() => {
+    setCartProducts(null);
+    setCartTotalQty(0);
+    localStorage.setItem("eShopCartItem" , JSON.stringify(null));
+  } ,[cartProducts])
+
   const value = {
     cartTotalQty,
     cartProducts,
     handleAddProductToCart,
     handleRemoveProductFromCart,
     handleCartQtyIncrease,
-    handleCartQtyDecrease
+    handleCartQtyDecrease,
+    handleClearCart
   };
 
   return <CartContext.Provider value={value} {...props} />;
